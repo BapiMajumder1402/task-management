edacea5f5c54b993b7085e8f2804696de21fdbf3
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-10-30 16:47:00 +0530

    Client side project setup done with initial required packages

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+    </>
+  );
+};
+
+export default App;
